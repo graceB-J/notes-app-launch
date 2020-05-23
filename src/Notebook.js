e89a6948b78bf9e5f44fb07431bb091f74ba72b6
Grace <gb4yy@virginia.edu>
2020-05-23 18:48:53 -0400

    editing mode from accordion toggle DISPLAY ONLY

--- src/Notebook.js
+++ src/Notebook.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Notebook = props => {
   const { notes, removeNote } = props;
@@ -20,23 +21,35 @@ const Notebook = props => {
   );
 };
 
+
 const NotebookBody = props => {
   const rows = props.notes.map(
     (row, index) => {
       return (
-        <Card className="d-flex flex-column note">
-          <Card.Header as="h4" className="d-flex flex-row">
-            <span className="flex-grow-1">{row.title}</span>
-            <Button onClick={() => props.removeNote(index)} variant="outline-danger">
-              DELETE note
-            </Button>
-          </Card.Header>
-          <Card.Body>
-            <Card.Text>
-              {row.contents}
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        <Accordion>
+          <Card className="d-flex flex-column note">
+            <Card.Header as="h4" className="d-flex flex-row">
+              <span className="flex-grow-1">{row.title}</span>
+              <Button onClick={() => props.starNote(index)} variant="outline-primary">
+                MARK IMPORTANT
+              </Button>
+              <Accordion.Toggle as={Button} variant="outline-warning" eventKey="0">
+                EDIT
+              </Accordion.Toggle>
+              <Button onClick={() => props.removeNote(index)} variant="outline-danger">
+                DELETE
+              </Button>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Editing Mode</Card.Body>
+            </Accordion.Collapse>
+            <Card.Body>
+              <Card.Text>
+                {row.contents}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Accordion>
       )
     }
   );
